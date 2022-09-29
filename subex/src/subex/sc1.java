060d33fa5c509d3cbfa274c1f7a737322b09f122
gayatri <gayatri@SC9893L11.subexworld.org>
2022-09-29 12:40:09 +0530

    First Commit

---
+++ subex/src/subex/sc1.java
@@ -0,0 +1,23 @@
+package subex;
+
+import java.util.Scanner;
+
+public class sc1 {
+	int a,b,c;
+	public void show(int a)
+	{
+		System.out.println("subex");
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		
+		
+		sc.show(3);
+		
+		
+
+	}
+
+}
